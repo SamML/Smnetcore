9b822dcc00f8251e3397c325c93da3d38c567d52
SamML <samuel89m@gmail.com>
2018-06-24 17:50:51 +0200

    no message

---
+++ Smnetcore.Rene.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Smnetcore.Rene.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
